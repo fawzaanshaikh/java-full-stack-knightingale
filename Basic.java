575cf8c09820e9b53dacb59cc4376d9b44ac2c66
Fawzaan <fawzi20112001@gmail.com>
2021-07-10 17:39:10 +0530

    Added Basic.java and HelloWorld.java

---
+++ Basic.java
@@ -0,0 +1,12 @@
+package knightingale;
+
+public class Basic {
+	public static void main(String[] args) {
+		int i = 100;
+		int j = 100;
+		
+		int sum = i +  j;
+		
+		System.out.println("The sum is " + sum);
+	}
+}
