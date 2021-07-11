8fd33a650c99ac071c452e77e482659c381a7cbc
Fawzaan <fawzi20112001@gmail.com>
2021-07-11 21:26:01 +0530

    Added AreaOfATriangle.java

---
+++ AreaOfATriangle.java
@@ -0,0 +1,19 @@
+package knightingale;
+
+import java.util.Scanner;
+
+public class AreaOfATriangle {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the base of the triangle: ");
+		double base = sc.nextDouble();
+		
+		System.out.println("Enter the height of the triangle: ");
+		double height = sc.nextDouble();
+
+		double area = (base * height) / 2;
+		System.out.println("The area of the rectangle is: " + area);
+		
+		sc.close();
+	}
+}
