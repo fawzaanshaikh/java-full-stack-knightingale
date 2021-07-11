9c69b4d4d929ae66bd4f94125b060c88d723ca52
Fawzaan <fawzi20112001@gmail.com>
2021-07-11 21:07:36 +0530

    Added AreaOfARectangle.java

---
+++ AreaOfARectangle.java
@@ -0,0 +1,19 @@
+package knightingale;
+
+import java.util.Scanner;
+
+public class AreaOfARectangle {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please Enter the lenght of Reactangle : ");
+		
+		double lenght = sc.nextDouble();
+		System.out.println("Enter the width of the rectangle");
+		
+		double width = sc.nextDouble();
+		double sum = lenght * width;
+		System.out.println(" the area of the reactangle is : " + sum);
+		
+		sc.close();
+	}
+}
