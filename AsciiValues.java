4965e7b26812ba9ae2f8c53f5886c988422f1822
Fawzaan <fawzi20112001@gmail.com>
2021-07-11 21:05:26 +0530

    Added AsciiValues.java

---
+++ AsciiValues.java
@@ -0,0 +1,12 @@
+package knightingale;
+
+public class AsciiValues {
+	public static void main(String[] args) {
+		char character = 's';
+		int Ascii = character;
+		System.out.println("The ASCII value of " + character + " is: " + Ascii);
+		
+		int typeCastAscii = (int) character;
+		System.out.println("The ASCII value of " + character + " is: " + typeCastAscii);
+	}
+}
